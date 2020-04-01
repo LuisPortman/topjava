2aad7392fa43df06d7d7b883be3ac778f124299e
Dmitry Dmitriev <v-dmitd-gri@ae.com>
2020-04-01 16:05:01 +0300

    HW3. Added meals table in DB scripts. Implemented JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,10 +1,22 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
   ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO meals (user_id, description, calories, date_time) VALUES
+  (100000, 'Завтрак', 500, '2020-01-30 10:00:00'),
+  (100000, 'Обед', 1000, '2020-01-30 13:00:00'),
+  (100000, 'Ужин', 500, '2020-01-30 20:00:00'),
+  (100000, 'Еда на граничное значение', 100, '2020-01-31 00:00:00'),
+  (100000, 'Завтрак', 1000, '2020-01-31 10:00:00'),
+  (100000, 'Обед', 500, '2020-01-31 13:00:00'),
+  (100000, 'Ужин', 410, '2020-01-31 20:00:00'),
+  (100001, 'Админ ланч', 510, '2015-06-01 13:00:00'),
+  (100001, 'Админ ужин', 1500, '2015-06-01 20:00:00');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
