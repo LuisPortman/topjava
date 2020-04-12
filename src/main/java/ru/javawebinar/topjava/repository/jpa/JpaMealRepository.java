45f8180f27d8afa32f1743d62d103ceb79468006
Dmitry Dmitriev <v-dmitd-gri@ae.com>
2020-04-12 11:45:31 +0300

    HW4. Implemented JpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,79 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional
 public class JpaMealRepository implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private JpaUserRepository jpaUserRepository;
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        User ref = em.getReference(User.class, userId);
+        meal.setUser(ref);
+        if (meal.isNew()) {
+            em.persist(meal);
+            return meal;
+        } else if (get(meal.getId(), userId) == null){
+            return null;
+        } else {
+            return em.merge(meal);
+        }
+//        this.insertMeal = new SimpleJdbcInsert(jdbcTemplate)
+//                .withTableName("meals")
+//                .usingGeneratedKeyColumns("id");
+//
+//        this.jdbcTemplate = jdbcTemplate;
+//        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id", id)
+                .setParameter("user_id", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = em.createNamedQuery(Meal.BY_ID)
+                .setParameter("id", id)
+                .setParameter("user_id", userId)
+                .getResultList();
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Meal.ALL_SORTED, Meal.class)
+                .setParameter("user_id", userId)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return em.createNamedQuery(Meal.BETWEEN_INCLUSIVE, Meal.class)
+                .setParameter("user_id", userId)
+                .setParameter("start_date", startDate)
+                .setParameter("end_date", endDate)
+                .getResultList();
     }
 }
